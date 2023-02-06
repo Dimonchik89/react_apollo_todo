fd46b9d99af576f9fa9747fd4bccb3b3c58b5c14
Dimonchik89 <dima.music1989@gmail.com>
2023-02-06 16:19:57 +0200

    first

---
+++ src/components/TodoList.js
@@ -0,0 +1,46 @@
+import { VStack, Spinner } from "@chakra-ui/react"
+import TodoItem from "./TodoItem"
+import TotalCount from "./TotalCount"
+import { useQuery, useMutation } from "@apollo/client"
+import { ALL_TODO, DELETE_TODO, UPDATE_TODO } from "../apollo/todos"
+
+const TodoList = () => {
+    const { loading, error, data } = useQuery(ALL_TODO);
+    const [toggleTodo, {error: updateError}] = useMutation(UPDATE_TODO)
+    const [removeTodo, {error: removeError}] = useMutation(DELETE_TODO, {
+        update(cache, {data: {removeTodo}}) {
+            cache.modify({
+                fields: {
+                    allTodos(currentTodos = []) {
+                        return currentTodos.filter(todo => todo.__ref !== `Todo:${removeTodo.id}`)
+                    }
+                }
+            })
+        }
+    })
+
+    if(loading) {
+        return <Spinner/>
+    }
+
+    if(error || updateError || removeError) {
+        return <h2>Error...</h2>
+    }
+
+    return (
+        <>
+            <VStack spacing={2} mt={4}>
+                {data.todos.map((todo) => (
+                    <TodoItem
+                        key={todo.id}
+                        onToggle={toggleTodo}
+                        onDelete={removeTodo}
+                        {...todo}
+                    />
+                ))}
+            </VStack>
+            <TotalCount/>
+        </>
+    )
+}
+export default TodoList;
